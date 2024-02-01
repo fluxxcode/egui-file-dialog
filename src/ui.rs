fb57fbe93790e7e5f71beb3c9bd889ae17bcceb3
FluxxCode <info.fluxxcode@gmail.com>
2024-02-01 21:50:01 +0100

    Update ui.rs

--- src/ui.rs
+++ src/ui.rs
@@ -1,3 +1,5 @@
+/// Adds a dynamically sized button to the UI that can be enabled or disabled.
+/// Returns true if the button is clicked. Otherwise None is returned.
 pub fn button_enabled_disabled(ui: &mut egui::Ui, text: &str, enabled: bool) -> bool {
     if !enabled {
         let button = egui::Button::new(text)
@@ -12,6 +14,8 @@ pub fn button_enabled_disabled(ui: &mut egui::Ui, text: &str, enabled: bool) -> 
     ui.add(egui::Button::new(text)).clicked()
 }
 
+/// Adds a fixed sized button to the UI that can be enabled or disabled.
+/// Returns true if the button is clicked. Otherwise None is returned.
 pub fn button_sized_enabled_disabled(
     ui: &mut egui::Ui,
     size: egui::Vec2,
@@ -31,6 +35,7 @@ pub fn button_sized_enabled_disabled(
     ui.add_sized(size, egui::Button::new(text)).clicked()
 }
 
+/// Returns the fill color of disabled buttons
 #[inline]
 fn get_disabled_fill_color(ui: &egui::Ui) -> egui::Color32 {
     let c = ui.style().visuals.widgets.noninteractive.bg_fill;
