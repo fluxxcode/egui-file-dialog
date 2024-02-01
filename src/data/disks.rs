66650e48051a9460ae08c7d87f86d70a075df801
FluxxCode <info.fluxxcode@gmail.com>
2024-02-01 22:03:32 +0100

    Document disks.rs

--- src/data/disks.rs
+++ src/data/disks.rs
@@ -1,5 +1,8 @@
 use std::path::{Path, PathBuf};
 
+/// Wrapper above the sysinfo::Disk struct.
+/// Used for helper functions and so that more flexibility is guaranteed in the future if
+/// the names of the disks are generated dynamically.
 #[derive(Default, Clone, PartialEq, Eq)]
 pub struct Disk {
     mount_point: PathBuf,
@@ -7,6 +10,7 @@ pub struct Disk {
 }
 
 impl Disk {
+    /// Create a new Disk object based on the data of a sysinfo::Disk.
     pub fn from_sysinfo_disk(disk: &sysinfo::Disk) -> Self {
         Self {
             mount_point: disk.mount_point().to_path_buf(),
@@ -14,10 +18,12 @@ impl Disk {
         }
     }
 
+    /// Returns the mount point of the disk
     pub fn mount_point(&self) -> &Path {
         &self.mount_point
     }
 
+    /// Returns the display name of the disk
     pub fn display_name(&self) -> &str {
         &self.display_name
     }
@@ -38,12 +44,14 @@ impl Disk {
     }
 }
 
+/// Wrapper above the sysinfo::Disks struct
 #[derive(Default)]
 pub struct Disks {
     disks: Vec<Disk>,
 }
 
 impl Disks {
+    /// Creates a new Disks object with a refreshed list of the system disks.
     pub fn new_with_refreshed_list() -> Self {
         let disks = sysinfo::Disks::new_with_refreshed_list();
 
@@ -55,6 +63,8 @@ impl Disks {
         Self { disks: result }
     }
 
+    /// Very simple wrapper method of the disks .iter() method.
+    /// No trait is implemented since this is currently only used internal.
     pub fn iter(&self) -> std::slice::Iter<'_, Disk> {
         self.disks.iter()
     }
