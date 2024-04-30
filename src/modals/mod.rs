65daf82d374b82edd43dbc78deef98a3cb1b8908
fluxxcode <info.fluxxcode@gmail.com>
2024-04-30 23:35:54 +0200

    First idea of modal actions

--- src/modals/mod.rs
+++ src/modals/mod.rs
@@ -1,12 +1,18 @@
+use std::path::PathBuf;
+
 mod overwrite_file_modal;
 
 pub use overwrite_file_modal::OverwriteFileModal;
+
+pub enum ModalAction {
+    SaveFile(PathBuf),
+}
 
 pub enum ModalState {
     // If the modal is currently open and still waiting for user input
     Pending,
     // If the modal should be closed in the next frame
-    Close
+    Close(ModalAction),
 }
 
 pub trait FileDialogModal {
