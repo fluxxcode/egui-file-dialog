ec04596deb06e649791076b7011e0d82101e5317
fluxxcode <info.fluxxcode@gmail.com>
2024-07-03 22:52:13 +0200

    Update crate documentation in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@
 //! An easy-to-use and customizable file dialog (a.k.a. file explorer, file picker) for
 //! [egui](https://github.com/emilk/egui).
 //!
-//! The project is currently in a very early version. Some planned features are still missing
-//! and some improvements still need to be made.
-//!
 //! **Currently only tested on Linux and Windows!**
 //!
 //! Read more about the project: <https://github.com/fluxxcode/egui-file-dialog>
@@ -13,14 +10,24 @@
 //! ### Features
 //! - Select a file or a directory
 //! - Save a file (Prompt user for a destination path)
+//!   - Dialog to ask the user if the existing file should be overwritten
+//! - Select multiple files and folders at once (ctrl/shift + click)
+//! - Open the dialog in a normal or modal window
 //! - Create a new folder
+//! - Keyboard navigation
+//! - Option to show or hide hidden files and folders
 //! - Navigation buttons to open the parent or previous directories
 //! - Search for items in a directory
+//! - Add file filters the user can select from a dropdown
 //! - Shortcut for user directories (Home, Documents, ...) and system disks
+//! - Pin folders to the left sidebar
+//! - Manually edit the path via text
 //! - Customization highlights:
 //!   - Customize which areas and functions of the dialog are visible
-//!   - Multilingual support: Customize the text labels that the dialog uses
+//!   - Customize the text labels used by the dialog to enable multilingual support
 //!   - Customize file and folder icons
+//!   - Add custom quick access sections to the left sidebar
+//!   - Customize keybindings used by the file dialog
 //!
 //! ### A simple example
 //!
@@ -59,30 +66,43 @@
 //! }
 //! ```
 //!
+//! ### Keybindings
+//! Keybindings can be used in the file dialog for easier navigation. All keybindings can be configured from the backend with `FileDialogKeyBindings` and `FileDialog::keybindings`. \
+//! The following table lists all available keybindings and their default values.
+//! | Name | Description | Default |
+//! | --- | --- | --- |
+//! | submit | Submit the current action or open the currently selected folder | `Enter` |
+//! | cancel | Cancel the current action | `Escape` |
+//! | parent | Open the parent directory | `ALT` + `↑` |
+//! | back | Go back | `Mouse button 1` <br/> `ALT` + `←` <br/> `Backspace` |
+//! | forward | Go forward | `Mouse button 2` <br/> `ALT` + `→` |
+//! | reload | Reload the file dialog data and the currently open directory | `F5` |
+//! | new_folder | Open the dialog to create a new folder | `CTRL` + `N` |
+//! | edit_path | Text edit the current path | `/` |
+//! | home_edit_path | Open the home directory and start text editing the path | `~` |
+//! | selection_up | Move the selection one item up | `↑` |
+//! | selection_down | Move the selection one item down | `↓` |
+//! | select_all | Select every item in the directory when using the file dialog to select multiple files and folders | `CTRL` + `A` |
+//!
 //! ### Customization
-//!
 //! Many things can be customized so that the dialog can be used in different situations. \
-//! A few highlights of the customization are listed below.
-//! (More customization will be implemented in the future!)
-//!
+//! A few highlights of the customization are listed below. For all possible customization options, see the documentation on [docs.rs](https://docs.rs/egui-file-dialog/latest/egui_file_dialog/struct.FileDialog.html).
+//! 
 //! - Set which areas and functions of the dialog are visible using `FileDialog::show_*` methods
 //! - Update the text labels that the dialog uses. See [Multilingual support](#multilingual-support)
-//! - Customize file and folder icons using `FileDialog::set_file_icon`
-//!   (Currently only unicode is supported)
-//!
-//! Since the dialog uses the egui style to look like the rest of the application,
-//! the appearance can be customized with `egui::Style`.
-//!
-//! The following example shows how a file dialog can be customized. If you need to
-//! configure multiple file dialog objects with the same or almost the same options,
-//! it is a good idea to use `FileDialogConfig` and `FileDialog::with_config`
-//!
-//! ```
+//! - Customize file and folder icons using `FileDialog::set_file_icon` (Currently only unicode is supported)
+//! - Customize keybindings used by the file dialog using `FileDialog::keybindings`. See [Keybindings](#keybindings)
+//! 
+//! Since the dialog uses the egui style to look like the rest of the application, the appearance can be customized with `egui::Style` and `egui::Context::set_style`.
+//! 
+//! The following example shows how a single file dialog can be customized. \
+//! If you need to configure multiple file dialog objects with the same or almost the same options, it is a good idea to use `FileDialogConfig` and `FileDialog::with_config` (See `FileDialogConfig` on [docs.rs](https://docs.rs/egui-file-dialog/latest/egui_file_dialog/struct.FileDialogConfig.html)).
+//! ```rust
 //! use std::path::PathBuf;
 //! use std::sync::Arc;
-//!
+//! 
 //! use egui_file_dialog::FileDialog;
-//!
+//! 
 //! FileDialog::new()
 //!     .initial_directory(PathBuf::from("/path/to/app"))
 //!     .default_file_name("app.cfg")
@@ -90,26 +110,31 @@
 //!     .resizable(false)
 //!     .show_new_folder_button(false)
 //!     .show_search(false)
-//!     // Markdown and text files should use the "document with text (U+1F5B9)" icon
+//!     .show_path_edit_button(false)
+//!     // Add a new quick access section to the left sidebar
+//!     .add_quick_access("Project", |s| {
+//!         s.add_path("☆  Examples", "examples");
+//!         s.add_path("📷  Media", "media");
+//!         s.add_path("📂  Source", "src");
+//!     })
+//!     // Markdown files should use the "document with text (U+1F5B9)" icon
 //!     .set_file_icon(
 //!         "🖹",
-//!         Arc::new(|path| {
-//!             match path
-//!                 .extension()
-//!                 .unwrap_or_default()
-//!                 .to_str()
-//!                 .unwrap_or_default()
-//!             {
-//!                 "md" => true,
-//!                 "txt" => true,
-//!                 _ => false,
-//!             }
-//!         }),
+//!         Arc::new(|path| path.extension().unwrap_or_default() == "md"),
 //!     )
 //!     // .gitignore files should use the "web-github (U+E624)" icon
 //!     .set_file_icon(
 //!         "",
 //!         Arc::new(|path| path.file_name().unwrap_or_default() == ".gitignore"),
+//!     )
+//!     // Add file filters the user can select in the bottom right
+//!     .add_file_filter(
+//!         "PNG files",
+//!         Arc::new(|p| p.extension().unwrap_or_default() == "png"),
+//!     )
+//!     .add_file_filter(
+//!         "Rust source files",
+//!         Arc::new(|p| p.extension().unwrap_or_default() == "rs"),
 //!     );
 //! ```
 //!
@@ -150,6 +175,56 @@
 //!         // Use custom labels for German
 //!         Language::German => get_labels_german(),
 //!     };
+//! }
+//! ```
+//! 
+//! ### Persistent data
+//! The file dialog currently requires the following persistent data to be stored across multiple file dialog objects:
+//! 
+//! - Folders the user pinned to the left sidebar (`FileDialog::show_pinned_folders`)
+//! - If hidden files and folders should be visible (`FileDialog::show_hidden_option`)
+//! 
+//! If one of the above feature is activated, the data should be saved by the application. Otherwise, frustrating situations could arise for the user and the features would not offer much added value.
+//! 
+//! All data that needs to be stored permanently is contained in the `FileDialogStorage` struct. This struct can be accessed using `FileDialog::storage` or `FileDialog::storage_mut` to save or load the persistent data. \
+//! By default the feature `serde` is enabled, which implements `serde::Serialize` and `serde::Deserialize` for the objects to be saved. However, the objects can also be accessed without the feature enabled.
+//! 
+//! The following example shows how the data can be saved with [eframe](https://github.com/emilk/egui/tree/master/crates/eframe) and the `serde` feature enabled. \
+//! Checkout `examples/persistence` for the full example.
+//! 
+//! ```rust
+//! use egui_file_dialog::FileDialog;
+//! 
+//! struct MyApp {
+//!     file_dialog: FileDialog,
+//! }
+//! 
+//! impl MyApp {
+//!     pub fn new(cc: &eframe::CreationContext) -> Self {
+//!         let mut file_dialog = FileDialog::default();
+//! 
+//!         // Load the persistent data of the file dialog.
+//!         // Alternatively, you can also use the `FileDialog::storage` builder method.
+//!         if let Some(storage) = cc.storage {
+//!             *file_dialog.storage_mut() =
+//!                 eframe::get_value(storage, "file_dialog_storage").unwrap_or_default()
+//!         }
+//! 
+//!         Self {
+//!             file_dialog,
+//!         }
+//!     }
+//! }
+//! 
+//! impl eframe::App for MyApp {
+//!     fn save(&mut self, storage: &mut dyn eframe::Storage) {
+//!         // Save the persistent data of the file dialog
+//!         eframe::set_value(
+//!             storage,
+//!             "file_dialog_storage",
+//!             self.file_dialog.storage_mut(),
+//!         );
+//!     }
 //! }
 //! ```
 
