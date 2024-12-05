bda4cd5e47e727821b374f08eaf3104dddd1ef25
hacknus <33124824+hacknus@users.noreply.github.com>
2024-12-05 20:54:19 +0100

    Update src/information_panel.rs
    Co-authored-by: Jannis <55352293+fluxxcode@users.noreply.github.com>

--- src/information_panel.rs
+++ src/information_panel.rs
@@ -399,7 +399,7 @@ impl InformationPanel {
 
     fn display_meta_data(&self, ui: &mut Ui, id: egui::Id, width: f32, item: &DirectoryEntry) {
         egui::ScrollArea::vertical().id_salt(id.with("meta_data_scroll")).show(ui, |ui| {
-            egui::Grid::new("meta_data")
+            egui::Grid::new(id.with("meta_data_grid"))
                 .num_columns(2)
                 .striped(true)
                 .min_col_width(width)
