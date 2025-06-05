efd336003bcd525430db9715ec80c0f2c8321841
crumblingstatue <radiantstatue@gmail.com>
2025-06-05 10:58:34 +0200

    feat!: replace operation_id with custom user_data (#269)
    Using strings for a (most likely) finite set of operations doesn't
    feel very Rusty.
    
    A user could instead define their own enum that they can do an exhaustive
    match over. The problem is how to integrate this into the FileDialog API
    without making things convoluted with generic parameters and whatnot.
    
    This commit goes for an approach using std::any::Any, and downcasting
    the user data into the requested type when the user data is queried.

--- examples/multiple_actions.rs
+++ examples/multiple_actions.rs
@@ -21,17 +21,22 @@ impl MyApp {
     }
 }
 
+enum Operation {
+    PickA,
+    PickB,
+}
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Pick file a").clicked() {
                 self.file_dialog.pick_file();
-                self.file_dialog.set_operation_id("pick_a");
+                self.file_dialog.set_user_data(Operation::PickA);
             }
 
             if ui.button("Pick file b").clicked() {
                 self.file_dialog.pick_file();
-                self.file_dialog.set_operation_id("pick_b");
+                self.file_dialog.set_user_data(Operation::PickB);
             }
 
             ui.label(format!("Pick file a: {:?}", self.picked_file_a));
@@ -40,12 +45,10 @@ impl eframe::App for MyApp {
             self.file_dialog.update(ctx);
 
             if let Some(path) = self.file_dialog.picked() {
-                if self.file_dialog.operation_id() == Some("pick_a") {
-                    self.picked_file_a = Some(path.to_path_buf());
-                }
-
-                if self.file_dialog.operation_id() == Some("pick_b") {
-                    self.picked_file_b = Some(path.to_path_buf());
+                match self.file_dialog.user_data::<Operation>() {
+                    Some(Operation::PickA) => self.picked_file_a = Some(path.to_path_buf()),
+                    Some(Operation::PickB) => self.picked_file_b = Some(path.to_path_buf()),
+                    None => {}
                 }
             }
         });
